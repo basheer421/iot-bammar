b6fec754fe5c989ab711fe5cba85b94319a48b33
bush <bush@debian>
2025-04-20 20:36:45 -0400

    k3d setup

---
+++ p3/scripts/k-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubectl create namespace dev
+kubectl create namespace argocd
+
+# From official repo
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# To not use -n dev everytime - set default namespace context
+kubectl config set-context --current --namespace=dev
+
