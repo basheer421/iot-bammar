777c6f25c51fcdc3cf64ee9ca103c0fb5fd35655
basheer421 <basheer123b2@gmail.com>
2025-05-24 14:55:38 +0000

    init deployment works

--- p3/scripts/k-setup.sh
+++ p3/scripts/k-setup.sh
@@ -10,7 +10,7 @@ else
     echo "Cluster 'mycluster' does not exist. Creating..."
 fi
 
-k3d cluster create mycluster
+k3d cluster create mycluster --port 30080:30080@loadbalancer --port 8888:80@loadbalancer
 
 kubectl create namespace dev
 kubectl create namespace argocd
@@ -27,33 +27,55 @@ done
 echo Gettng argocd password...
 ARGOCD_PASSWORD=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)
 
+echo "Opening a NodePort 30080 service for argocd..."
+kubectl patch svc argocd-server -n argocd -p '{
+  "spec": {
+    "type": "NodePort",
+    "ports": [{
+      "port": 443,
+      "targetPort": 8080,
+      "nodePort": 30080,
+      "protocol": "TCP"
+    }]
+  }
+}'
+
 # To not use -n dev everytime - set default namespace context
 kubectl config set-context --current --namespace=dev
+
+echo "Installing Ingress NGINX (baremetal variant)..."
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/baremetal/deploy.yaml
+
+echo "Waiting for Ingress NGINX controller pod to be ready..."
+kubectl wait --namespace ingress-nginx \
+  --for=condition=Ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=60s
+
+echo "Waiting for Ingress webhook service to have endpoints..."
+until kubectl get endpoints ingress-nginx-controller-admission -n ingress-nginx -o jsonpath='{.subsets[*].addresses[*].ip}' | grep -q .; do
+  echo "Waiting for ingress-nginx-controller-admission endpoints..."
+  sleep 2
+done
 
 echo Applyig Dev project
 kubectl apply -f ../confs/conf.yml
 
-echo Opening port 8080 for argocd login...
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
-PF_PID=$!
-sleep 5 # To make sure the port is opened
-
-# Trap to kill port-forward on script exit or interrupt
-trap "kill $PF_PID" EXIT
+echo Ensuring argocd is ready for logging in...
+kubectl wait deployment argocd-server -n argocd --for=condition=Available=True --timeout=60s
 
 echo Loging in to argocd
-argocd login localhost:8080 --username admin --password "$ARGOCD_PASSWORD" --insecure
+argocd login localhost:30080 --username admin --password "$ARGOCD_PASSWORD" --insecure
 
 echo 'Creating argocd app from (https://github.com/basheer421/iot-bammar.git)...'
-argocd app create my-app \
+argocd app create iot \
   --repo https://github.com/basheer421/iot-bammar.git \
   --path p3/confs \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace dev
 
 echo Syncing the app with argocd...
-argocd app sync my-app
+argocd app sync iot
 
 echo Waiting for app to become synced and healthy...
-argocd app wait my-app --sync --health --timeout 60
-
+argocd app wait iot --sync --health --timeout 60
