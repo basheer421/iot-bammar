a7484a8d93ecb593379a7146483b8f79b3c7e6a4
basheer421 <basheer123b2@gmail.com>
2025-06-02 16:39:09 +0000

    fix argocd peridoc refresh

--- p3/scripts/k-setup.sh
+++ p3/scripts/k-setup.sh
@@ -68,13 +68,13 @@ echo Loging in to argocd
 argocd login localhost:30080 --username admin --password "$ARGOCD_PASSWORD" --insecure
 
 echo 'Patching Argo CD controller to enable periodic Git polling...'
-kubectl patch deployment argocd-application-controller -n argocd \
+kubectl patch deployment argocd-applicationset-controller -n argocd \
   --type json \
   -p '[
     {"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--status-refresh-time=10s"},
     {"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--repo-server-refresh-timeout=10s"}
   ]'
-kubectl rollout restart deployment argocd-application-controller -n argocd
+kubectl rollout restart deployment argocd-applicationset-controller -n argocd
 
 echo 'Creating argocd app from (https://github.com/basheer421/iot-bammar.git)...'
 argocd app create iot \
@@ -90,3 +90,10 @@ argocd app sync iot
 
 echo Waiting for app to become synced and healthy...
 argocd app wait iot --sync --health --timeout 60
+
+
+echo Your login:
+echo ---------------
+echo Username: admin
+echo Password: $ARGOCD_PASSWORD
+echo ---------------
