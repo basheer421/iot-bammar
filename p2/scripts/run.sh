fb9616d4ca4f595863f18b7321c305792c7d9476
basheer421 <basheer123b2@gmail.com>
2025-07-21 21:26:23 +0400

    feat: update Vagrant configuration and add Kubernetes deployment files

--- p2/scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install -y curl
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--bind-address=192.168.56.110 --flannel-iface=eth1" sh -
-cp /vagrant/scripts/three_app.yaml /vagrant/
-sudo kubectl apply -f /vagrant/three_app.yaml
